04d8326362fdbc9095f90b173a35504b0491a39c
dtaylor-us <derektaylor.us@gmail.com>
2019-10-12 14:41:04 -0500

    06 initialize db

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,12 +1,20 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [app.theme :refer [cheffy-theme]]
+            [re-frame.core :as rf]
+            [app.db]
+            ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
 
 (defn app
   []
-  [:div "Cheffy"])
+  [:<>
+   [:> Normalize]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [:> Button "hello"]]])
 
 (defn ^:dev/after-load start
   []
+  (rf/dispatch-sync [:initialize-db])
   (r/render [app]
     (.getElementById js/document "app")))
 
