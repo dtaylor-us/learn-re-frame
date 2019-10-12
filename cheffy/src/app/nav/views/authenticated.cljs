99b972c25539cec45dc167485dcd02c62ec9aedb
dtaylor-us <derektaylor.us@gmail.com>
2019-10-12 16:21:57 -0500

    08 nav-events

--- cheffy/src/app/nav/views/authenticated.cljs
+++ cheffy/src/app/nav/views/authenticated.cljs
@@ -1,24 +1,30 @@
 (ns app.nav.views.authenticated
   (:require [app.nav.views.nav-item :refer [nav-item]]
+            [re-frame.core :as rf]
             ["@smooth-ui/core-sc" :refer [Box]]))
 
 (defn authenticated
   []
   (let [nav-items [{:id :saved
                     :name "Saved"
-                    :href "#saved"}
+                    :href "#saved"
+                    :dispatch #(rf/dispatch [:set-active-nav :saved])}
                    {:id :recipes
                     :name "Recipes"
-                    :href "#recipes"}
-                   {:id :inbox
+                    :href "#recipes"
+                    :dispatch #(rf/dispatch [:set-active-nav :recipes])}
+                   {:id :inboxes
                     :name "Inbox"
-                    :href "#inbox"}
+                    :href "#inbox"
+                    :dispatch #(rf/dispatch [:set-active-nav :inboxes])}
                    {:id :become-a-chef
                     :name "Chef"
-                    :href "#become-a-chef"}
+                    :href "#become-a-chef"
+                    :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
                    {:id :profile
                     :name "Profile"
-                    :href "#profile"}]]
+                    :href "#profile"
+                    :dispatch #(rf/dispatch [:set-active-nav :profile])}]]
     [:> Box {:display "flex"
              :justify-content "flex-end"
              :py 1}
