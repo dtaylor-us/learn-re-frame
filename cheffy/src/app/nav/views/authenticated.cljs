398680107a79d3726f20df97313c8e2f8a4e6113
dtaylor-us <derektaylor.us@gmail.com>
2019-10-13 01:46:06 -0500

    09 nav subs

--- cheffy/src/app/nav/views/authenticated.cljs
+++ cheffy/src/app/nav/views/authenticated.cljs
@@ -5,28 +5,32 @@
 
 (defn authenticated
   []
-  (let [nav-items [{:id :saved
-                    :name "Saved"
-                    :href "#saved"
-                    :dispatch #(rf/dispatch [:set-active-nav :saved])}
-                   {:id :recipes
-                    :name "Recipes"
-                    :href "#recipes"
-                    :dispatch #(rf/dispatch [:set-active-nav :recipes])}
-                   {:id :inboxes
-                    :name "Inbox"
-                    :href "#inbox"
-                    :dispatch #(rf/dispatch [:set-active-nav :inboxes])}
-                   {:id :become-a-chef
-                    :name "Chef"
-                    :href "#become-a-chef"
-                    :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
-                   {:id :profile
-                    :name "Profile"
-                    :href "#profile"
-                    :dispatch #(rf/dispatch [:set-active-nav :profile])}]]
-    [:> Box {:display "flex"
-             :justify-content "flex-end"
-             :py 1}
-     (for [item nav-items]
-       [nav-item item])]))
+  (let [active-nav @(rf/subscribe [:active-nav])
+        nav-items  [{:id       :saved
+                     :name     "Saved"
+                     :href     "#saved"
+                     :dispatch #(rf/dispatch [:set-active-nav :saved])}
+                    {:id       :recipes
+                     :name     "Recipes"
+                     :href     "#recipes"
+                     :dispatch #(rf/dispatch [:set-active-nav :recipes])}
+                    {:id       :inboxes
+                     :name     "Inbox"
+                     :href     "#inbox"
+                     :dispatch #(rf/dispatch [:set-active-nav :inboxes])}
+                    {:id       :become-a-chef
+                     :name     "Chef"
+                     :href     "#become-a-chef"
+                     :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
+                    {:id       :profile
+                     :name     "Profile"
+                     :href     "#profile"
+                     :dispatch #(rf/dispatch [:set-active-nav :profile])}]]
+    [:>
+     Box
+     {:display         "flex"
+      :justify-content "flex-end"
+      :py              1}
+     (for [{:keys [id name href dispatch]} nav-items]
+       [nav-item
+        {:id id :key id :name name :href href :dispatch dispatch :active-nav active-nav}])]))
