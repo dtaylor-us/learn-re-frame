f1b4da6eb2d2746a3a464a79181d030c159d9397
dtaylor-us <derektaylor.us@gmail.com>
2019-10-18 22:32:25 -0500

    15 log in event-fx

--- cheffy/src/app/auth/views/log_in.cljs
+++ cheffy/src/app/auth/views/log_in.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
             [app.components.page-nav :refer [page-nav]]
+            [app.components.form-group :refer [form-group]]
             ["@smooth-ui/core-sc" :refer [Row Col FormGroup Label Input Box Button]]))
 
 (defn log-in
@@ -12,20 +13,14 @@
       [:> Row {:justify-content "center"}
        [:> Col {:xs 12 :sm 6}
         [page-nav {:center "Log in"}]
-        [:> FormGroup
-         [:> Label {:html-for :email} "Email"]
-         [:> Input {:control true
-                    :id :email
-                    :type "email"
-                    :value (:email @values)
-                    :on-change #(swap! values assoc :email (.. % -target -value))}]]
-        [:> FormGroup
-         [:> Label {:html-for :password} "Password"]
-         [:> Input {:control true
-                    :id :password
-                    :type "password"
-                    :value (:password @values)
-                    :on-change #(swap! values assoc :password (.. % -target -value))}]]
+        [form-group {:id :email
+                     :label "Email"
+                     :type "email"
+                     :values values}]
+        [form-group {:id :password
+                     :label "Password"
+                     :type "password"
+                     :values values}]
         [:> Box {:display "flex"
                  :justify-content "space-between"}
          [:> Box {:py 1
@@ -34,5 +29,5 @@
                :on-click #(rf/dispatch [:set-active-nav :sign-up])}
            "New to Cheffy? Create an account!"]]
          [:> Box
-          [:> Button {:on-click #(js/console.log "log-in")}
+          [:> Button {:on-click #(rf/dispatch [:log-in @values])}
            "Log in"]]]]])))
